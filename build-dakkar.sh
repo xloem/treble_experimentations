5e22396be59867e807f33e16c028743614cf97ae
xloem <0xloem@gmail.com>
2022-07-19 10:26:56 -0400

    Reference my user before phhusson's

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -382,6 +382,7 @@ function clone_or_checkout() {
             git checkout origin/"$localManifestBranch"
         )
     else
+    	git clone https://github.com/xloem/"$repo" "$dir" -b "$localManifestBranch" ||
         git clone https://github.com/phhusson/"$repo" "$dir" -b "$localManifestBranch"
     fi
 }
